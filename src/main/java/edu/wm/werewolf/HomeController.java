2af315cab8749f009f57dd1b56a450be2d496354
brittanyrey <brittanyrey@gmail.com>
2013-10-17 14:59:47 -0400

    gameDAO issues

--- src/main/java/edu/wm/werewolf/HomeController.java
+++ src/main/java/edu/wm/werewolf/HomeController.java
@@ -47,7 +47,7 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/addUser", method=RequestMethod.POST)
-	public void newUser(@ModelAttribute("user") User user)
+	public void newUser(@RequestParam("user") User user)
 	{
 		logger.info("Adding new user : " + user.getFirstName() + user.getHashedPassword() + user.getId()
 				+user.getLastName());
