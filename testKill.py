7760afaf3f2455242cb733842454f8a5c93bb63d
brittanyrey <brittanyrey@gmail.com>
2013-10-27 14:15:26 -0400

    near

--- testKill.py
+++ testKill.py
@@ -8,62 +8,62 @@ password = "yes"
 
 #Players Near
 payload = {'userID' : '3393'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', 
 	data = payload, auth=HTTPBasicAuth(username, password))
 print("User ID: 3393---players near " + r.text)
 
 payload = {'userID' : '555'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 555---players near " + r.text)
 
 payload = {'userID' : '942'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 942---players near " + r.text)
 
 payload = {'userID' : '111'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', 
 	data = payload, auth=HTTPBasicAuth(username, password))
 print("User ID: 111---players near " + r.text)
 
 payload = {'userID' : '222'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 222---players near " + r.text)
 
 payload = {'userID' : '444'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 444---players near " + r.text)
 
 payload = {'userID' : '666'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', 
 	data = payload, auth=HTTPBasicAuth(username, password))
 print("User ID: 666---players near " + r.text)
 
 payload = {'userID' : '777'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 777---players near " + r.text)
 
 payload = {'userID' : '888'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 888---players near " + r.text)
 
 payload = {'userID' : '1234'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', 
 	data = payload, auth=HTTPBasicAuth(username, password))
 print("User ID: 1234---players near " + r.text)
 
 payload = {'userID' : '555'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 555---players near " + r.text)
 
 payload = {'userID' : '900'}
-requests.post('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
+r = requests.get('http://mighty-sea-1005.herokuapp.com/players/near', data = payload, 
 			auth=HTTPBasicAuth(username, password))
 print("User ID: 900---players near " + r.text)
 
